e56dfd53a531d079816fe5b4bce70c049c0c58d5
elboyko <boyko.lena@gmail.com>
2023-12-21 20:38:42 +0700

    add task

---
+++ script.js
@@ -0,0 +1,26 @@
+// делаем get запрос на сервер для получения постов
+fetch('https://jsonplaceholder.typicode.com/posts')
+   .then((res) => res.json())
+   .then((posts) => {
+      const container = document.querySelector(".post-container");
+      posts.forEach((post) => {
+         const element = createPostElement(post);
+         appendElementToContainer(container, element);
+      });
+   })
+
+
+// Функция, которая принимает объект поста и возвращает строку HTML-разметки
+function createPostElement(post) {
+   return `
+      <div class="post">
+      <h2>Заголовок: ${post.title}</h2>
+      <p>Статья: ${post.body}</p>
+      </div>
+   `;
+}
+
+// Функция, которая добавляет разметку в контейнер
+function appendElementToContainer(container, element) {
+   container.innerHTML += element;
+}
